1e571aaace22c0a2d025c614bfa4f07649241e85
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2023-11-19 19:19:32 +0530

    adding expense

---
+++ ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+function ExpenseItem(){
+    return  (
+        <div>
+        <h2>Expense item</h2>
+        <p>food Rs 10</p>
+        <p>Petrol Rs100</p>
+        <p>Movies Rs200</p>
+        </div>
+    );
+    
+}
+
+export default ExpenseItem;
