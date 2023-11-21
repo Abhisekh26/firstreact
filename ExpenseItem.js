9b6f7343d09404aff3b028664781dc723f4ca52b
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2023-11-21 17:22:24 +0530

    props2

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,11 +1,29 @@
 import React from "react";
-function ExpenseItem(){
+import './ExpenseItem.css'
+function ExpenseItem(props){
+    // var date="November 19"
+    // const LocationofExpenditure= "Patna"
+    // var exp1="Food"
+    // var exp1Price=10
+    // var exp2="Petrol"
+    // var exp2Price=100
+    // var exp3="Movies"
+    // var exp3Price=200
+
     return  (
-        <div>
-        <h2>Expense item</h2>
-        <p>food Rs 10</p>
-        <p>Petrol Rs100</p>
-        <p>Movies Rs200</p>
+        <div className="expense-item">
+            <div>{props.Date}</div>
+             <h2>Expense item</h2>
+             <h2>{props.City}</h2>
+        <div className="expense-item__description">
+        <div className="expense-item">
+                    {props.Food}     </div>
+        <div className="expense-item__price"> Rs{props.Cost}</div>
+        <div className="expense-item">
+                    {props.Fuel}      </div>
+        <div className="expense-item__price">Rs{props.FuelCost}</div>
+        
+        </div>
         </div>
     );
     
