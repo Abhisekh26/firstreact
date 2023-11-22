5308160c059c5ae5c8daa9cf754fcf2aca340eba
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2023-11-22 12:17:22 +0530

    Add files via upload
    uodated expense item component 10

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,30 +1,24 @@
 import React from "react";
 import './ExpenseItem.css'
+import ExpenseTime from "./ExpenseItemTime";
 function ExpenseItem(props){
-    // var date="November 19"
-    // const LocationofExpenditure= "Patna"
-    // var exp1="Food"
-    // var exp1Price=10
-    // var exp2="Petrol"
-    // var exp2Price=100
-    // var exp3="Movies"
-    // var exp3Price=200
-
     return  (
         <div className="expense-item">
-            <div>{props.Date}</div>
-             <h2>Expense item</h2>
+            
+        <ExpenseTime date={props.date}/>
+
+             <h2>{props.title}</h2>
              <h2>{props.City}</h2>
-        <div className="expense-item__description">
-        <div className="expense-item">
-                    {props.Food}     </div>
+            <div className="expense-item__description">
+            <div className="expense-item">
+                    {props.Expenses}     
+                    </div>
         <div className="expense-item__price"> Rs{props.Cost}</div>
-        <div className="expense-item">
-                    {props.Fuel}      </div>
-        <div className="expense-item__price">Rs{props.FuelCost}</div>
+    
+       
+        </div>
+        </div>
         
-        </div>
-        </div>
     );
     
 }
