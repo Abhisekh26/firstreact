9a03befb529ba19e9189b6d9f8eda8de9c4a4f4f
Abhisekh26 <142305216+Abhisekh26@users.noreply.github.com>
2023-12-06 21:19:13 +0530

    adding components

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,28 +1,35 @@
 import React,{useState} from "react";
 import './ExpenseItem.css'
 import ExpenseTime from "./ExpenseItemTime";
+//import ExpensesForm from "./ExpensesForm"
 const ExpenseItem=(props)=>{
+    let[setCost,SetNewCost]=useState("")
+     let [Cost,newCost]= useState(props.Cost);
 
-    let [Cost,newCost]= useState(props.Cost);
-
-    var xyz=()=>{
-        
-        newCost("100")
-     
+    var xyz=()=> {
+        newCost(setCost)
+        console.log(setCost)
     }
+    var SetPrice=(event)=> SetNewCost(event.target.value)
+     
     return  (
         <div className="expense-item">
-            
-        <ExpenseTime date={props.date}/>
+              <ExpenseTime date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <h2>{props.City}</h2>
-                 <h2> {props.Expenses}    </h2> 
-                
-        <div className="expense-item__price"> Rs{Cost}</div>
-        </div>
-        <button onClick={xyz}>Change Price</button>
+                <h2> {props.Expenses} </h2> 
+                <div className="expense-item__price"> Rs{Cost}</div>
+                 </div>
        
+            <input type="text"  ></input>
+            <h2>Add Expense</h2>
+            <input type="text" value={setCost} onChange={SetPrice}></input>
+            <h2>Price</h2>
+           <input type="text"></input>
+           <h2>Date</h2> 
+            <button onClick={xyz}>Submit</button>  
+    
+      
+        
     
         </div>
         
